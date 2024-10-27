8ada58763116d3e309fed529b93b83af9babe92b
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-10-27 22:07:25 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -11,12 +11,10 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
-  // </React.StrictMode>
 );
 reportWebVitals();
